d9230105e896aa2e7ae9f57fb8838e77246c8141
kmcooper <kmcooper@unomaha.edu>
2021-05-26 09:44:11 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+###############################
+# 
+# Download the original dataset
+# from the USDA and put the files
+# that come with it in a static archive
+#
+# NOTE: You will want to uncomment these
+# commands so they can run
+#
+###############################
+mkdir static_archive
+cd static_archive
+# wget https://fdc.nal.usda.gov/fdc-datasets/FoodData_Central_branded_food_csv_2020-10-30.zip
+# unzip FoodData_Central_branded_food_csv_2020-10-30.zip
+# chmod a-w *
+# cd ../
